eb610bbc111e2a16816fb57338bf41384069741a
Abhinash Gupta <abhiofficial453@gmail.com>
2026-01-08 16:00:32 +0530

    .

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Header from "./components/header";
-import Footer from "./components/footer"
-import Chatbot from "./components/chatbot"
+import Footer from "./components/Footer";
+import Chatbot from "./components/chatbot";
 
 export default function RootLayout({ children }) {
   return (
